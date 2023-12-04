1ced5d0f03d9a8e0c6504c5f92a8538ac8277ab0
Dongmin Kim <ddmkim94@gmail.com>
2023-12-04 12:58:19 +0900

    Feat: 프로젝트 생성 및 초기 설정
    - .gitignore 파일 수정
    - querydsl 설정 코드 추가

---
+++ src/test/java/com/api/bookinside/BookinsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bookinside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookinsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
