f3c83d9720bd25a508f287554ffb0f8f16c599ad
Dongmin Kim <ddmkim94@gmail.com>
2023-12-04 14:57:16 +0900

    Feat: 시큐리티 설정 파일 작성
    개발 편의를 위해 초기에는 모든 요청에 대한 인증을 해제함.
    `PasswordEncoder`도 시큐리티 설정 파일에 작성

---
+++ src/main/java/com/api/bookinside/security/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.api.bookinside.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
