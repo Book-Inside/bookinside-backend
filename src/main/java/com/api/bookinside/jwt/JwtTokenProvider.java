8a71972c080c13f2ae5a8ea1be824b4d56e74873
Dongmin Kim <ddmkim94@gmail.com>
2023-12-05 14:12:27 +0900

    Remove: Optiomize Imports
    불필요한 import 제거

--- src/main/java/com/api/bookinside/jwt/JwtTokenProvider.java
+++ src/main/java/com/api/bookinside/jwt/JwtTokenProvider.java
@@ -3,7 +3,6 @@ package com.api.bookinside.jwt;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
